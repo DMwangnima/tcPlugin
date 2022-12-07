1b8963532beb395604a8dbe2e37d05aed1c8f874
GitHub Action <action@github.com>
2022-12-07 08:02:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/dmwangnima/tcplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/dmwangnima/tcplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.dmwangnima.tcplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.dmwangnima.tcplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/dmwangnima/tcplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/dmwangnima/tcplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.dmwangnima.tcplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.dmwangnima.tcplugin.MyBundle
 
 class MyApplicationService {
 
